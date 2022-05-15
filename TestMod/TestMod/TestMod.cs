23859e39920add13dcee2102124738662f427035
Veg <60745091+Veggieoskibroski@users.noreply.github.com>
2022-05-14 23:54:25 -0500

    Some usable code (mostly blank)

---
+++ TestMod/TestMod/TestMod.cs
@@ -0,0 +1,22 @@
+﻿using Celeste.Mod.UI;
+using Celeste;
+using Celeste.Mod;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celeste.Mod.TestMod
+{
+    public class TestMod : EverestModule
+    {
+        void Load()
+        { }
+
+        void Unload()
+        { }
+
+    }
+}
