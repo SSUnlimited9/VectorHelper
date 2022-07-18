02ff0bf17355fdd87bbfe26494f98f128cca66ec
SSUnlimited <Tinytt333@users.noreply.github.com>
2022-07-17 19:15:06 -0500

    Added holdableObj class file and ahorn launch

---
+++ VectorHelper/Entities/CustomHoldable.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Celeste.Mod.Entities;
+using Monocle;
+
+namespace Celeste.Mod.VectorHelper.Entities
+{
+    [Tracked(true)]
+    [CustomEntity("VectorHelper/CustomHoldable")]
+    public class CustomHoldable
+    {
+    }
+}
