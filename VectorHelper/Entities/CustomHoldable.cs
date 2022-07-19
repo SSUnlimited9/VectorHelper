7f26e8367d8dbeb724f82eb9b3756c6d8d0435f9
SSUnlimited <Tinytt333@users.noreply.github.com>
2022-07-19 00:30:15 -0500

    Update CustomHoldable.cs

--- VectorHelper/Entities/CustomHoldable.cs
+++ VectorHelper/Entities/CustomHoldable.cs
@@ -1,19 +1,11 @@
 using System;
 using System.Collections.Generic;
-using Microsoft.Xna.Framework;
 using Celeste.Mod.Entities;
 using Monocle;
 
 namespace Celeste.Mod.VectorHelper.Entities
 {
-    [Tracked(true)]
+    [Tracked(false)]
     [CustomEntity("VectorHelper/CustomHoldable")]
-    public class CustomHoldable : Actor
-    {
-        public string spriteDirectory {get; set;}
-
-        public CustomHoldable(Vector2 position) : base(position) {
-            
-        }
-    }
+    public class CustomHoldable {}
 }
