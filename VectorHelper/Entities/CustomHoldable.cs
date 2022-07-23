6d96b8aa6682d7d69410ece412d9cd59c1d80c5c
Veg <60745091+Veggieoskibroski@users.noreply.github.com>
2022-07-23 14:31:42 -0500

    Updated CustomHoldable.cs

--- VectorHelper/Entities/CustomHoldable.cs
+++ VectorHelper/Entities/CustomHoldable.cs
@@ -14,17 +14,20 @@ namespace Celeste.Mod.VectorHelper.Entities
 
         public Image HoldableSprite; /* The sprite for rendering with "Image" */
 
-        public Holdable Hold; /* Do I really need to comment this? */
+        public Holdable Hold; /* Do I really need to comment this? */ /* yes */
 
         public Player player;
 
         
-        public CustomHoldable(EntityData data, Vector2 offset) : base(data.Position + offset){}
+        public CustomHoldable(EntityData data, Vector2 offset) : this(data.Position + offset, data.Attr("spriteDirectory","objects/resortclutter/yellow_14")){
 
-        public CustomHoldable(Vector2 position, string spriteDirectory) : base(position)
+        }
+
+        public CustomHoldable(Vector2 position, string _spriteDir) : base(position)
         {
             Depth = 100;
             Collider = new Hitbox(12, 12, 0, 0); /* This collider size is temporary */
+            spriteDirectory = _spriteDir;
             MTexture holdableSprite = GFX.Game[spriteDirectory];
             Add(HoldableSprite = new Image(holdableSprite));
         }
