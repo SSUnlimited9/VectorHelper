9139cbc9754b60add827fc5d785b9a132f6103ba
Veg <60745091+Veggieoskibroski@users.noreply.github.com>
2022-08-01 20:37:52 -0500

    Updated a few random things (unfinished)

---
+++ Loenn/triggers/CustomHoldableSpawn.lua
@@ -0,0 +1,18 @@
+--VectorHelper Custom Holdable Spawner (Loenn Integration)
+--Written by KtheVeg
+
+local CustomHoldableSpawner = {}
+
+CustomHoldableSpawner.name = "VectorHelper/CustomHoldableSpawner"
+
+
+CustomHoldableSpawner.data = {
+    {
+        name = "Custom Holdable Spawner",
+        data = {
+            customHoldableData = ""
+        }
+    }
+}
+
+return CustomHoldableSpawner
