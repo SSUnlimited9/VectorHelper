1c9666807718fa6d0e8fe5d1b947a85715495426
Veg <60745091+Veggieoskibroski@users.noreply.github.com>
2022-08-01 21:24:55 -0500

    reworking custom holdable

--- Loenn/entities/CustomHoldable.lua
+++ Loenn/entities/CustomHoldable.lua
@@ -3,24 +3,30 @@
 
 local customHoldable = {}
 
-customHoldable.name = "VectorHelper/CustomHoldableBasic"
-customHoldable.depth = 100
+customHoldable.name = "VectorHelper/BasicCustomHoldable"
 customHoldable.justification = {0.5, 0.5}
 customHoldable.fieldOrder = {
-    "x", "y", "spriteDirectory", "killPlayerOnDestroy"
+    "x", "y", "spriteDirectory", "spriteOffset", "entityDepth", "linkedToPlayer", "slowsPlayerDown"
 }
 customHoldable.placements = {
     {
         name = "Custom Holdable",
         data = {
             spriteDirectory = "objects/resortclutter/yellow_14",
-            killPlayerOnDestroy = false
+            spriteOffset = "0,0",
+            entityDepth = 100,
+            linkedToPlayer = false,
+            slowsPlayerDown = false
         }
     }
 }
 
 function customHoldable.texture(room, entity)
     return entity.spriteDirectory
+end
+
+function customHoldable.depth(room, entity)
+    return entity.entityDepth
 end
 
 
--- VectorHelper/Triggers/Triggers.HoldableSpawner.cs
+++ VectorHelper/Triggers/Triggers.HoldableSpawner.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Celeste.Mod.Entities;
+using Celeste;
+using Microsoft.Xna.Framework;
+using Monocle;
 namespace Celeste.Mod.VectorHelper.Triggers
 {
-    public class HoldableSpawner
+    [CustomEntity("VectorHelper/CustomHoldableSpawner")]
+    public class HoldableSpawner : Trigger
     {
-        
+        public HoldableSpawner(EntityData _entityData, Vector2 _offset) : base(_entityData, _offset) {
+
+        }
     }
 }
