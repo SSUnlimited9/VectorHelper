245210b9373855cb928f11f4c2a9b2748f81ea4b
Veg <60745091+Veggieoskibroski@users.noreply.github.com>
2022-08-02 19:18:55 -0500

    Made Sprite Offset

--- Loenn/entities/CustomHoldable.lua
+++ Loenn/entities/CustomHoldable.lua
@@ -1,12 +1,27 @@
 -- VectorHelper Custom Holdable (Loenn Integration)
 -- Written by KtheVeg
 
+--Loenn Libs
+local drawableSprite = require "structs.drawable_sprite"
+local drawableRectangle = require "structs.drawable_rectangle"
+
+function Split (inputstr, sep)
+        if sep == nil then
+                sep = "%s"
+        end
+        local t={}
+        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+                table.insert(t, str)
+        end
+        return t
+end
+
 local customHoldable = {}
 
 customHoldable.name = "VectorHelper/BasicCustomHoldable"
 customHoldable.justification = {0.5, 0.5}
 customHoldable.fieldOrder = {
-    "x", "y", "spriteDirectory", "spriteOffset", "visualDepth", "linkedToPlayer", "slowsPlayerDown"
+    "x", "y", "spriteDirectory", "spriteOffset", "visualDepth", "linkedToPlayer", "slowsPlayerDown", "spawnId", "interactionId"
 }
 customHoldable.placements = {
     {
@@ -23,8 +38,15 @@ customHoldable.placements = {
     }
 }
 
-function customHoldable.texture(room, entity)
-    return entity.spriteDirectory
+function customHoldable.sprite(room, entity)
+    local spriteOverrides = entity
+    spriteOverrides.x, spriteOverrides.y = (entity.x + (Split(entity.spriteOffset, ",")[1] or 0)) or 0, (entity.y + (Split(entity.spriteOffset, ",")[2] or 0)) or 0
+    spriteOverrides.spriteDirectory = entity.spriteDirectory or "objects/resortclutter/yellow_14"
+    local sprites = {}
+
+    table.insert(sprites, drawableSprite.fromTexture(spriteOverrides.spriteDirectory, spriteOverrides))
+
+    return sprites
 end
 
 function customHoldable.depth(room, entity)
